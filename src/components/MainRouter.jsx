d3128465d23801f04f2b2ff3c67183396704b6e4
FMGordillo <facundomgordillo@gmail.com>
2019-11-10 01:34:22 -0300

    Added fading styling + <nojavascript>

--- src/components/MainRouter.jsx
+++ src/components/MainRouter.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
+import { Route, Switch, useHistory } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 import Landing from "../pages";
 import Info from "../pages/info";
@@ -19,27 +20,49 @@ import "../styles/style.css";
 import { fetchUser, getAllUsers } from "../redux/actions/index";
 // import { useAuth0 } from "../react-auth0-spa"
 
+const duration = 300;
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0
+};
+
+const transitionStyles = {
+  entering: { opacity: 1 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 0 },
+  exited: { opacity: 0 }
+};
+
 function MainRouter(props) {
+  const { location } = useHistory();
   return (
-    <Switch>
-      <Route exact path="/" component={Landing} />
-      {/*Decidir cual landing va a ir si LandingForm de compornents o landing de pages*/}
-      <Route exact path="/info" component={Info} />
-      <Route exact path="/about" component={About} />
-      <PrivateRoute exact path="/social" component={Social} />
-      <PrivateRoute exact path="/profile" component={Profile} />
-      <PrivateRoute
-        exact
-        path="/register"
-        render={({ history }) => <MainForm history={history} />}
-      />
-      {/* TODO */}
-      <PrivateRoute exact path="/register/proyect" component={MainProj} />
-      {/* TODO */}
-      <PrivateRoute exact path="/listComp" component={ListaComponent} />
-      <PrivateRoute exact path="/haveNeed" component={HaveNeed} />
-      <PrivateRoute exact path="/project" component={Project} />
-    </Switch>
+    <TransitionGroup>
+      <CSSTransition
+        key={location.key}
+        timeout={duration}
+        classNames="react-transition"
+      >
+        <Switch location={location}>
+          <Route exact path="/" component={Landing} />
+          {/*Decidir cual landing va a ir si LandingForm de compornents o landing de pages*/}
+          <Route exact path="/info" component={Info} />
+          <Route exact path="/about" component={About} />
+          <PrivateRoute exact path="/social" component={Social} />
+          <PrivateRoute exact path="/profile" component={Profile} />
+          <PrivateRoute
+            exact
+            path="/register"
+            render={({ history }) => <MainForm history={history} />}
+          />
+          {/* TODO */}
+          <PrivateRoute exact path="/register/proyect" component={MainProj} />
+          {/* TODO */}
+          <PrivateRoute exact path="/listComp" component={ListaComponent} />
+          <PrivateRoute exact path="/haveNeed" component={HaveNeed} />
+          <PrivateRoute exact path="/project" component={Project} />
+        </Switch>
+      </CSSTransition>
+    </TransitionGroup>
   );
 }
 
